b7cda91a772d28e7df198df6aec20094f3a6eff9
Vinícius Borges <99985671+viniciusgborges@users.noreply.github.com>
2022-02-19 00:49:15 -0300

    Update ReviewRenderer.php

--- Block/Product/ReviewRenderer.php
+++ Block/Product/ReviewRenderer.php
@@ -9,39 +9,17 @@ use Magento\Review\Model\ReviewSummaryFactory;
 use Magento\Review\Observer\PredispatchReviewObserver;
 use Magento\Framework\View\Element\Template;
 
-/**
- * Class ReviewRenderer
- */
 class ReviewRenderer extends \Magento\Review\Block\Product\ReviewRenderer implements ReviewRendererInterface
 {
-    /**
-     * Array of available template name
-     *
-     * @var array
-     */
     protected $_availableTemplates = [
         self::FULL_VIEW => 'Magento_Review::helper/summary.phtml',
         self::SHORT_VIEW => 'Magento_Review::helper/summary_short.phtml',
     ];
-
-    /**
-     * Review model factory
-     *
-     * @var \Magento\Review\Model\ReviewFactory
-     */
+    
     protected $_reviewFactory;
 
-    /**
-     * @var ReviewSummaryFactory
-     */
     private $reviewSummaryFactory;
 
-    /**
-     * @param \Magento\Framework\View\Element\Template\Context $context
-     * @param \Magento\Review\Model\ReviewFactory $reviewFactory
-     * @param array $data
-     * @param ReviewSummaryFactory $reviewSummaryFactory
-     */
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         \Magento\Review\Model\ReviewFactory $reviewFactory,
@@ -54,11 +32,6 @@ class ReviewRenderer extends \Magento\Review\Block\Product\ReviewRenderer implem
         Template::__construct($context, $data);
     }
 
-    /**
-     * Review module availability
-     *
-     * @return string
-     */
     public function isReviewEnabled(): string
     {
         return $this->_scopeConfig->getValue(
@@ -67,16 +40,7 @@ class ReviewRenderer extends \Magento\Review\Block\Product\ReviewRenderer implem
         );
     }
 
-    /**
-     * Get review summary html
-     *
-     * @param Product $product
-     * @param string $templateType
-     * @param bool $displayIfNoReviews
-     *
-     * @return string
-     * @throws \Magento\Framework\Exception\LocalizedException
-     * @throws \Magento\Framework\Exception\NoSuchEntityException
+    throws \Magento\Framework\Exception\NoSuchEntityException
      */
     public function getReviewsSummaryHtml(
         \Magento\Catalog\Model\Product $product,
@@ -106,34 +70,18 @@ class ReviewRenderer extends \Magento\Review\Block\Product\ReviewRenderer implem
         return $this->toHtml();
     }
 
-    /**
-     * Get ratings summary
-     *
-     * @return string
-     */
     public function getRatingSummary()
     {
         $this->_reviewFactory->create()->getEntitySummary($this->getProduct(), $this->_storeManager->getStore()->getId());
         return $this->getProduct()->getRatingSummary()->getRatingSummary();
     }
 
-    /**
-     * Get count of reviews
-     *
-     * @return int
-     */
     public function getReviewsCount()
     {
         $this->_reviewFactory->create()->getEntitySummary($this->getProduct(), $this->_storeManager->getStore()->getId());
         return $this->getProduct()->getRatingSummary()->getReviewsCount();
     }
 
-    /**
-     * Get review product list url
-     *
-     * @param bool $useDirectLink allows to use direct link for product reviews page
-     * @return string
-     */
     public function getReviewsUrl($useDirectLink = false)
     {
         $product = $this->getProduct();
